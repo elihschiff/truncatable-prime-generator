7f217d361c1c32595f8e16b797897f07b9518620
Eli <elihschiff@gmail.com>
2018-07-30 22:34:31 -0400

    added a html display

--- find-primes.js
+++ find-primes.js
@@ -4,7 +4,6 @@ var largestNumberChecked = 2;
 var truncatablePrimes = []
 
 
-
 function addPrimesToList(maxNumberToCheck){//addes primes to the list of primes. You must pass it the largest number you want it to check when finding primes
 	for(var i=largestNumberChecked+1;i<=maxNumberToCheck;i++){//loops starting at the largestNumberChecked and checks every number to see if it is prime
 		for(var j=0;j<primes.length;j++){
@@ -14,7 +13,6 @@ function addPrimesToList(maxNumberToCheck){//addes primes to the list of primes.
 				}
 			}else{
 				primes.push(i)
-				// console.log(i)
 				break
 			}
 		}
@@ -26,38 +24,43 @@ function addPrimesToList(maxNumberToCheck){//addes primes to the list of primes.
 function getTruncatedPrime(basePrime, lengthToCheck){//baseprime is a know truncatable prime or 0. length is the length of the longest truncatable prime you want to find or -1 to find them all
 	for(var i=0;i<10;i++){
 		numberToCheck = parseInt(basePrime + "" + i)
-		// console.log(parseInt(basePrime + "" + i))
 
-		addPrimesToList(numberToCheck)
+		addPrimesToList(numberToCheck)//gets the prime numbers needed to check the current number
 
 
 		if(primes.includes(numberToCheck)){
 			console.log("New Truncatable Prime: " + numberToCheck) 
 			truncatablePrimes.push(numberToCheck)
+			truncatablePrimes.sort(function(a, b){return a - b});//sorts the array of truncatable primes becasue my function does not put them in order
+
 
 
 			if(numberToCheck.toString().length<lengthToCheck || lengthToCheck == -1){
-				// console.log()
 				getTruncatedPrime(numberToCheck,lengthToCheck)
-
 			}
 		}
-
-
-
-
 	}
 
 }
 
-function run(lengthToCheck){
+function displayPrimes(){
+	var htmlPrimeList = document.getElementById("prime-list")
+	htmlPrimeList.innerHTML =  ""
+	for(var i=0;i<truncatablePrimes.length;i++){
+		var node = document.createElement("p");
+		var textnode = document.createTextNode(truncatablePrimes[i]);
+		node.appendChild(textnode);
+		htmlPrimeList.appendChild(node);
+	}
+}
+
+function run(){
 	primes = [2] 
 	largestNumberChecked = 2;
 	truncatablePrimes = []
+	lengthToCheck = document.getElementById('itterationCount').value
 
 
 	getTruncatedPrime(0, lengthToCheck)
-
-	truncatablePrimes.sort(function(a, b){return a - b});//sorts the array of truncatable primes becasue my function does not put them in order
-	console.log(truncatablePrimes)
+	displayPrimes()
 }
